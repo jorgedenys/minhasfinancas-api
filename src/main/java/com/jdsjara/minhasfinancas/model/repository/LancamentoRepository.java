515501cd3b5da5c92a088ea507633ae35477a543
Jorge Denys <jorgedenys@gmail.com>
2022-12-29 20:18:14 -0300

    finalizando o projeto

--- src/main/java/com/jdsjara/minhasfinancas/model/repository/LancamentoRepository.java
+++ src/main/java/com/jdsjara/minhasfinancas/model/repository/LancamentoRepository.java
@@ -7,11 +7,24 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.jdsjara.minhasfinancas.model.entity.Lancamento;
+import com.jdsjara.minhasfinancas.model.enums.StatusLancamento;
 import com.jdsjara.minhasfinancas.model.enums.TipoLancamento;
  
 public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
 	
-	@Query(value = "SELECT sum(l.valor) FROM Lancamento l join l.usuario u WHERE u.id = :idUsuario and l.tipo =:tipo GROUP BY u")
-	BigDecimal obterSaldoPorTipoLancamentoEUsuario(@Param("idUsuario") Long idUsuario, @Param("tipo") TipoLancamento tipo);
+	@Query( value =
+			" SELECT "
+		  + " sum(l.valor) "
+		  + " FROM Lancamento l"
+		  + " join l.usuario u"
+		  + " WHERE u.id = :idUsuario"
+		  + "   and l.tipo = :tipo "
+		  + "   and l.status = :status "
+		  + " GROUP BY u "
+		  )
+	BigDecimal obterSaldoPorTipoLancamentoEUsuarioEStatus(
+			@Param("idUsuario") Long idUsuario,
+			@Param("tipo") TipoLancamento tipo,
+			@Param("status") StatusLancamento status);
 
 }
--- src/main/java/com/jdsjara/minhasfinancas/resource/LancamentoResource.java
+++ src/main/java/com/jdsjara/minhasfinancas/resource/LancamentoResource.java
@@ -35,6 +35,13 @@ public class LancamentoResource {
 	private final LancamentoService service;
 	private final UsuarioService usuarioService;
 	
+	@GetMapping("{id}")
+	public ResponseEntity obterLancamento( @PathVariable("id") Long id ) {
+		return service.obterPorId(id)
+					.map( lancamento -> new ResponseEntity(converter(lancamento), HttpStatus.OK) )
+					.orElseGet( () -> new ResponseEntity(HttpStatus.NOT_FOUND) );
+	}
+	
 	@PostMapping
 	public ResponseEntity salvar( @RequestBody LancamentoDTO dto ) {
 		
@@ -124,8 +131,21 @@ public class LancamentoResource {
 		
 		return ResponseEntity.ok(lancamentos);
 		
-	 }
+	}
 	
+	private LancamentoDTO converter(Lancamento lancamento) {
+		return LancamentoDTO.builder()
+					.id(lancamento.getId())
+					.descricao(lancamento.getDescricao())
+					.valor(lancamento.getValor())
+					.mes(lancamento.getMes())
+					.ano(lancamento.getAno())
+					.status(lancamento.getStatus().name())
+					.tipo(lancamento.getTipo().name())
+					.idUsuario(lancamento.getUsuario().getId())
+					.build();
+	}
+		
 	private Lancamento converter(LancamentoDTO dto) {
 		
 		Lancamento lancamento = new Lancamento();
